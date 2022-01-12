13d38b363f2a6ed91471e2c0b8d8da2090d6fd2e
Dav0x0 <david1.aprilio4@gmail.com>
2022-01-12 22:15:17 +0700

    update route web

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Davidaprilio\LaravelStarter\Http\Controllers;
 use Davidaprilio\LaravelStarter\Http\Controllers\Api;
 
-Route::get('/photo', [Api\ImageController::class, 'resize'])->name('user');
+
+Route::middleware('web')->group(function () {
+
+    Route::get('/login', [Controllers\AuthenticationController::class, 'show'])->name('login');
+    Route::post('/login', [Controllers\AuthenticationController::class, 'store']);
+    Route::post('/logout', [Controllers\AuthenticationController::class, 'destroy'])->name('logout');
+
+    Route::get('/photo', [Api\ImageController::class, 'resize'])->name('user');
+});
